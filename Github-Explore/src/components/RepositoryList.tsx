4e0ce5540dc24c66ef661ea92f12b907dcb8189d
Gislaine Jessica <gislainehjessica@gmail.com>
2021-07-08 21:42:09 -0300

    feat: config typescript

--- Github-Explore/src/components/RepositoryList.tsx
+++ Github-Explore/src/components/RepositoryList.tsx
@@ -8,8 +8,14 @@ const repository = {
   link: "https://github.com/gislainejessica/blog-chacomigo"
 }
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
