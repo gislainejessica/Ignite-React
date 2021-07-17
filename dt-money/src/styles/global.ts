55ec1620773e55dda6655c1e0b02e0edc00a6c30
Gislaine Jessica <gislainehjessica@gmail.com>
2021-07-17 17:56:34 -0300

    header component struture and css

--- dt-money/src/styles/global.ts
+++ dt-money/src/styles/global.ts
@@ -24,19 +24,27 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  // font-size: 16px (Desktop)
   html {
     @media (max-width: 1080px) {
-      font-size: 93.75%; // 15px
+      font-size: 93.75%; 
     }
     @media (max-width: 720px) {
-      font-size: 87.5%;  // 14px
+      font-size: 87.5%;  
     }
   }
 
   body {
     background: var(--background-color);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
