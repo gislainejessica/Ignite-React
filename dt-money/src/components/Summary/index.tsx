d139c4538cb54e3aa6c8f25830c633c194f22114
Gislaine Jessica <gislainehjessica@gmail.com>
2021-07-19 18:55:22 -0300

    dt-money transactions table

--- dt-money/src/components/Summary/index.tsx
+++ dt-money/src/components/Summary/index.tsx
@@ -1,9 +1,38 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
 import { Container } from './styles'
 
 export function Summary() {
   return (
     <Container>
-      <h2> Summmary </h2>
+      <div>
+        <header>
+          <p> Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+
+        <strong> R$ 1000,00 </strong>
+      </div>
+
+      <div>
+        <header>
+          <p> Saidas </p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+
+        <strong> - R$ 500,00 </strong>
+      </div>
+
+      <div className="highlight">
+        <header>
+          <p> Total </p>
+          <img src={totalImg} alt="Total" />
+        </header>
+
+        <strong> R$ 500,00 </strong>
+      </div>
     </Container>
   )
 }
