9eea5123f2a8db05a65b7fc6027fb30ef683b815
Gislaine Jessica <gislainehjessica@gmail.com>
2021-07-22 16:59:02 -0300

    modal styles

--- dt-money/src/components/NewTransationModal/index.tsx
+++ dt-money/src/components/NewTransationModal/index.tsx
@@ -1,6 +1,6 @@
-import Modal from 'react-modal';
-import { Container } from './styles';
-
+import Modal from 'react-modal'
+import { Container } from './styles'
+import closeImg from '../../assets/close.svg'
 interface NewTransationModalProps {
   isOpen: boolean
   onRequestClose: () => void
@@ -14,15 +14,25 @@ export function NewTransationModal({ isOpen, onRequestClose }: NewTransationModa
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
       <Container>
-        <h2> Cadastrar Trasnsação </h2>
+        <h2> Cadastrar Transação </h2>
         <input placeholder="Titulo" />
         <input type="number" placeholder="Valor" />
         <input placeholder="Categoria" />
-        <button type="submit">
 
+        <button type="submit">
+          Cadastrar
         </button>
       </Container>
+
     </Modal>
 
   )
